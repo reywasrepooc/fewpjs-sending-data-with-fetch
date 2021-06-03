60366ebb41914fa1ffeb41e82e617b313e274986
Cooper Sawyer <cooper.sawyer@powerhrg.com>
2021-06-03 15:24:25 -0400

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Add your code here
+const submitData = (userName, userEmail) => {
+ return fetch ('http://localhost:3000/users', {
+    method: 'POST',
+    headers: {
+        'Content-Type': 'application/json',
+        'Accept': 'application/json'
+    },
+    body: JSON.stringify({
+        name: userName,
+        email: userEmail
+    })
+    })  
+    .then(resp => resp.json())
+    .then(data => {
+        const bodies = document.body
+         bodies.append(data.id)
+    })
+    .catch(chicken => {
+    const bodyError = document.body
+       bodyError.append(chicken.message)
+    })
+
+}
